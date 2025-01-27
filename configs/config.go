4cd1de4cc0ebd68f43ff3bba9a1e895e9d55d3f5
altynbek07 <altynbek.kazezov.97@gmail.com>
2025-01-27 23:26:26 +0500

    Add configuration loading and environment variable management

---
+++ configs/config.go
@@ -0,0 +1,38 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Db   DbConfig
+	Auth AuthConfig
+}
+
+type DbConfig struct {
+	Dsn string
+}
+
+type AuthConfig struct {
+	Secret string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		panic(err)
+	}
+
+	return &Config{
+		Db: DbConfig{
+			Dsn: os.Getenv("DSN"),
+		},
+		Auth: AuthConfig{
+			Secret: os.Getenv("SECRET"),
+		},
+	}
+}
