d62d152b0510434d307b8baa87410de09117c446
altynbek07 <altynbek.kazezov.97@gmail.com>
2025-01-27 19:06:51 +0500

    Add environment variables, update .gitignore, and configure Docker for PostgreSQL and Mailpit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func App() *http.ServeMux {
+	router := http.NewServeMux()
+
+	return router
+}
+
+func main() {
+	app := App()
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: app,
+	}
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
 }
