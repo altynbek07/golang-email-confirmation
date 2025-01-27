b8fa15a851cb766b76ad95d8462c64c97c2aabfe
altynbek07 <altynbek.kazezov.97@gmail.com>
2025-01-27 23:28:12 +0500

    feat: add repository interfaces for user and statistics management

---
+++ pkg/di/interfaces.go
@@ -0,0 +1,14 @@
+package di
+
+import (
+	"go/email-confirmation/internal/user"
+)
+
+type IStatRepository interface {
+	AddClick(linkId uint)
+}
+
+type IUserRepository interface {
+	Create(user *user.User) (*user.User, error)
+	FindByEmail(email string) (*user.User, error)
+}
