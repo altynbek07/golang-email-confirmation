cbe006c3833ee04574f6dd34a7e91203a97668eb
altynbek07 <altynbek.kazezov.97@gmail.com>
2025-01-27 23:28:31 +0500

    refactor: remove unused IStatRepository interface from DI package

--- pkg/di/interfaces.go
+++ pkg/di/interfaces.go
@@ -4,10 +4,6 @@ import (
 	"go/email-confirmation/internal/user"
 )
 
-type IStatRepository interface {
-	AddClick(linkId uint)
-}
-
 type IUserRepository interface {
 	Create(user *user.User) (*user.User, error)
 	FindByEmail(email string) (*user.User, error)
