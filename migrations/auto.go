c2838e3ba3ab144c50a4ce935d1eee320f2b6a61
altynbek07 <altynbek.kazezov.97@gmail.com>
2025-01-28 00:27:15 +0500

    feat: add migration script for user model and database connection setup

---
+++ migrations/auto.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"go/email-confirmation/internal/user"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		panic(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(generateDsn()), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&user.User{})
+}
+
+func generateDsn() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_SSLMODE"))
+}
